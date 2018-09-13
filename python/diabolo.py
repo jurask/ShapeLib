c43d0cd561257698581e148b1a0aace1c4aad163
jxjl <jxjl@seznam.cz>
2018-09-13 14:22:39 +0200

    Bowtie and diabolo: added handles

--- python/diabolo.py
+++ python/diabolo.py
@@ -15,32 +15,55 @@ class Diabolo(pya.PCellDeclarationHelper):
     self.param("wb", self.TypeDouble, "Bridge width", default = 0.1)
     self.param("lw", self.TypeDouble, "Wing length", default = 0.1)
     self.param("a", self.TypeDouble, "Wing angle", default = 45)
+    self.param("hb", self.TypeShape, "", default = pya.DPoint(0.05, 0.05))
+    self.param("hw", self.TypeShape, "", default = pya.DPoint(0.15, 0.09142135623730951))
+    self.param("lb_", self.TypeDouble, "Bridge length", default = 0.1, hidden = True)
+    self.param("wb_", self.TypeDouble, "Bridge width", default = 0.1, hidden = True)
+    self.param("lw_", self.TypeDouble, "Wing length", default = 0.1, hidden = True)
+    self.param("a_", self.TypeDouble, "Wing angle", default = 45, hidden = True)
 
   def display_text_impl(self):
     # Provide a descriptive text for the cell
     return "Diabolo(lb=" + str('%.3f' % self.lb) + ",wb=" + ('%.3f' % self.wb) +  ",lw=" + ('%.3f' % self.lw) + ",angle=" + ('%.3f' % self.a) +")"
   
   def coerce_parameters_impl(self):
-  
-    # We employ coerce_parameters_impl to decide whether the handle or the 
-    # numeric parameter has changed (by comparing against the effective 
-    # radius ru) and set ru to the effective radius. We also update the 
-    # numerical value or the shape, depending on which on has not changed.
-    pass
+    if self.lb < 0:
+      self.lb *= -1
+    if self.wb < 0:
+      self.wb *= -1
+    if self.lw < 0:
+      self.lw *= -1
+    if self.lb != self.lb_ or self.wb != self.wb_ or self.lw != self.lw_ or self.a != self.a_:
+      # update handle
+      hbx = self.lb/2
+      hby = self.wb/2
+      hwx = hbx + self.lw
+      hwy = hby + self.lw * math.tan(math.radians(self.a/2))
+      self.hb = pya.DPoint(hbx, hby)
+      self.hw = pya.DPoint(hwx, hwy)
+      # fix params
+      self.lb_ = self.lb
+      self.wb_ = self.wb
+      self.lw_ = self.lw
+      self.a_ = self.a
+    else:
+      # calc params from handle
+      hbx = abs(self.hb.x)
+      hby = abs(self.hb.y)
+      hwx = abs(self.hw.x)
+      hwy = abs(self.hw.y)
+      self.lb = self.lb_ = hbx * 2
+      self.wb = self.wb_ = hby * 2
+      self.lw = self.lw_ = hwx - hbx
+      self.a = self.a_ = 2 * math.degrees(math.atan((hwy - hby)/self.lw))
   
   def can_create_from_shape_impl(self):
-    # Implement the "Create PCell from shape" protocol: we can use any shape which 
-    # has a finite bounding box
     return False
   
   def parameters_from_shape_impl(self):
-    # Implement the "Create PCell from shape" protocol: we set r and l from the shape's 
-    # bounding box width and layer
     return None
   
   def transformation_from_shape_impl(self):
-    # Implement the "Create PCell from shape" protocol: we use the center of the shape's
-    # bounding box to determine the transformation
     return pya.Trans(self.shape.bbox().center())
   
   def produce_impl(self):
@@ -51,7 +74,7 @@ class Diabolo(pya.PCellDeclarationHelper):
     dbu = self.layout.dbu
     tg = math.tan(math.radians(self.a/2))
     
-    # compute the circle
+    # compute the diabolo
     pts = []
     pts.append(pya.Point(pya.DPoint((-self.lb/2)/dbu, (self.wb/2)/dbu)))
     pts.append(pya.Point(pya.DPoint((-self.lb/2-self.lw)/dbu, (self.wb/2+self.lw*tg)/dbu)))
